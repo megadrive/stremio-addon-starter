44b2bb78c65ca0be23fe39213f9b532cb3608570
Andy <a.simeon@gmail.com>
2025-03-05 12:52:25 +1100

    removed remnants of astro, finished with base styles

--- packages/web/src/components/ConfigureForm.tsx
+++ packages/web/src/components/ConfigureForm.tsx
@@ -2,52 +2,122 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ConfigSchema, type Config, config } from "@stremio-addon/config";
 import { useState } from "react";
+import { cn } from "@/lib/utils";
+
+function generateInstallLinks(userConfig: Config):
+  | {
+      /** encoded config */
+      config: string;
+      /** manifest url, used for desktop. */
+      url: string;
+      /** install link for web */
+      web: string;
+    }
+  | undefined {
+  try {
+    const encodedConfig = config.encode(userConfig);
+    // attempt to decode, if we can't then no bueno
+    if (!config.decode(encodedConfig)) {
+      throw new Error("Invalid config");
+    }
+
+    const path = `/${encodedConfig}/manifest.json`;
+
+    // if running locally in dev, use http, otherwise use stremio protocol
+    const protocol = window.location.origin.startsWith("http://localhost:4321")
+      ? "http"
+      : "stremio";
+    const url = `${protocol}://${window.location.host}${path}`;
+    const web = `https://web.stremio.com/#/addons?addon=${encodeURIComponent(url)}`;
+
+    return {
+      config: encodedConfig,
+      url,
+      web,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+
+  return undefined;
+}
 
 export default function ConfigureForm() {
   const {
     register,
     handleSubmit,
+    formState,
     formState: { errors },
   } = useForm<Config>({
     resolver: zodResolver(ConfigSchema),
     defaultValues: {
       variable1: "",
     },
+    mode: "onChange",
   });
 
-  const [configString, setConfigString] = useState<string>();
+  const [configString, setConfigString] =
+    useState<ReturnType<typeof generateInstallLinks>>();
 
+  // handle form submission
   const onSubmit = handleSubmit((data) => {
-    const encodedConfig = config.encode(data);
-    console.log({ data, encodedConfig });
-
-    setConfigString(encodedConfig);
+    const generated = generateInstallLinks(data);
+    setConfigString(generated);
   });
 
   return (
     <form onSubmit={onSubmit}>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-1">
         <div className="flex flex-col gap-2">
-          <label htmlFor="variable1">Variable 1</label>
-          <span className="text-red-500">{errors.variable1?.message}</span>
-          <input type="text" {...register("variable1")} />
+          <label htmlFor="variable1">
+            Variable 1
+            <span className="text-red-500">{errors.variable1?.message}</span>
+          </label>
+
+          <input
+            type="text"
+            {...register("variable1")}
+            className="border-2 border-gray-300 rounded-md p-2"
+          />
         </div>
         <div>
-          <button type="submit">Submit</button>
+          <button
+            type="submit"
+            className={cn([
+              "bg-slate-500",
+              "hover:bg-slate-700",
+              "text-white",
+              "py-2",
+              "px-4",
+              "rounded",
+              "focus:outline-none",
+              "focus:shadow-outline",
+              "cursor-pointer",
+            ])}
+          >
+            Submit
+          </button>
         </div>
       </div>
-      <div className={`${configString ? "" : "hidden"} flex flex-col gap-2`}>
+      <div
+        className={`${configString && !formState.isDirty ? "" : "hidden"} flex flex-row gap-6`}
+      >
         <a
-          href={`/${configString}/manifest.json`.replace(/^https?/i, "stremio")}
+          href={configString?.url ?? ""}
+          className="bg-slate-500 text-white p-2 rounded"
         >
           Install
         </a>
-        <a
-          href={`https://web.stremio.com/#/addons?addon=${configString}/manifest.json`}
-        ></a>
+        <a href={configString?.web ?? ""} className="p-2">
+          Install (web)
+        </a>
         <a
           href="#"
-          onClick={() => navigator.clipboard.writeText(configString ?? "")}
+          className="p-2"
+          onClick={(e) => {
+            e.preventDefault();
+            navigator.clipboard.writeText(configString?.url ?? "");
+          }}
         >
           Copy
         </a>
